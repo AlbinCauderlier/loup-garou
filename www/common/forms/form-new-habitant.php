4655e472241eef8d4f9b16d64f7627b6ba5edf2a
Albin CAUDERLIER <albin.cauderlier@gmail.com>
2020-03-22 19:07:01 +0100

    Update form-new-habitant.php

--- www/common/forms/form-new-habitant.php
+++ www/common/forms/form-new-habitant.php
@@ -13,52 +13,43 @@
                                 </div>
                                 <div class="col-md-4">
                                     <h3>Choisir un joueur</h3>
-                                    <div class="form-row">
-                                        <div class="col-md-4">
-                                            <div class="input-group mb-2">
-                                                <div class="input-group-prepend">
-                                                    <div class="input-group-text">
-                                                        <i data-feather="user"></i>
-                                                    </div>
-                                                </div>
-                                                <select id="user" name="user-profile" class="form-control custom-select border-left-0" required>
-                                                    <option disabled selected>Profile</option>
-                                                <?php
-                                                    $users = json_decode(callAPI('GET',API_URL.'/api/users/'), true);
-                                                    foreach( $users as $user ){
-                                                        
-                                                        echo('<option value="'.$user["user-id"].'">'.$user["user-firstname"].'</option>');
-                                                    }
-                                                ?>
-                                                </select>
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">
+                                                <i data-feather="user"></i>
                                             </div>
                                         </div>
+                                        <select id="user" name="user-profile" class="form-control custom-select border-left-0" required>
+                                            <option disabled selected>Joueur</option>
+                                        <?php
+                                            $users = json_decode(callAPI('GET',API_URL.'/api/users/'), true);
+                                            foreach( $users as $user ){
+                                                
+                                                echo('<option value="'.$user["user-id"].'">'.$user["user-firstname"].'</option>');
+                                            }
+                                        ?>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
-                                    <h3>Choisir un rôle</h3>
-                                    <div class="form-row">
-                                    <?php
-                                                echo('<div class="input-group mb-2">');
-                                                    echo('<div class="input-group-prepend">');
-                                                        echo('<div class="input-group-text">');
-                                                            echo('<i data-feather="tag"></i>');
-                                                        echo('</div>');
-                                                    echo('</div>');
-                                                    echo('<select id="user-profile" name="'.$user["user-id"].'-user-profile" class="form-control custom-select border-left-0" required>');
-                                                        echo('<option disabled selected>Profile</option>');
-                                                        echo('<option value="storyteller">Conteur</option>');
-                                                        echo('<option value="citizen">Villageois</option>');
-                                                        echo('<option value="werewolf">Loup Garou</option>');
-                                                        echo('<option value="witch">Sorcière</option>');
-                                                        echo('<option value="littlegirl">Petite Fille</option>');
-                                                        echo('<option value="hunter">Chasseur</option>');
-                                                        echo('<option value="cupid">Cupidon</option>');
-                                                    echo('</select>');
-                                                echo('</div>');
-                                            echo('</div>');
-                                        }
-                                    ?>
+                                    <h3>Choisir une carte</h3>
+                                    <div class="input-group mb-2">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">
+                                                <i data-feather="tag"></i>
+                                            </div>
+                                        </div>
+                                        <select id="user-card" name="user-card" class="form-control custom-select border-left-0" required>
+                                            <option disabled selected>Carte</option>
+                                            <option value="storyteller">Conteur</option>
+                                            <option value="citizen">Villageois</option>
+                                            <option value="werewolf">Loup Garou</option>
+                                            <option value="witch">Sorcière</option>
+                                            <option value="littlegirl">Petite Fille</option>
+                                            <option value="hunter">Chasseur</option>
+                                            <option value="cupid">Cupidon</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                             <button class="btn btn-gradient p-2 mb-5" type="submit">
